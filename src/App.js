048ff3597582d61f461cad4a8f84c8ac6514883c
AdamsAdinatu <adinatu23@gmail.com>
2022-08-17 18:16:26 +0000

    created react form

--- src/App.js
+++ src/App.js
@@ -1,87 +1,13 @@
-import { render } from '@testing-library/react';
-import React , {component} from 'react'
-import profilecard from './components/profilecard'
+import React from 'react'
+import ProfileForm from './components/profileform'
+import ProfileCaard from './components/ProfileCard'
 
-class App extends component {
-  constructor(){
-  supper();
-
-  this.handleclick=this.handleclick.bind(this)
-
-  this.state={
-    writer:{
-      londing:false,
-      list:[]
-    }
-  };
-
-this.handleclick(){
-  this.setstate({
-    writer:{
-      loading:true,
-    }
-  });
-  setTimeout(async()=> {
-    let resp=await("/writers.json");
-    let result =await resp.json()
-
-  })
-
-  this.setstate({
-    writers:{
-      loading:false,
-      list:result
-    }
-  });
-
-
-
-  }
-
+function App() {
+  return (
+    <div>
+    
+    </div>
+  )
 }
 
- render(){
-  const {
-    writers:{loading,  list}
-  }=this.state;
-
-  
- }
-
- if(loading){
- return (
-
-    <div>
-    <h1>writers profilr</h1>
-    <div className="container">
-    <div className="card action">
-    <p className="infotext"> loading...</p>
-
-    </div> 
-
-    </div>
-    </div>
-  )
- }
-
- 
-   
-} 
-return (
-
-  <div>
-  <h1>writers profilr</h1>
-  <div className="container">
-  list.length === 0 ?
-    
-    
-  <div className="card action">
-  <p className="infotext"> oops...no writer profile found</p>
-  <button className= 'actionBtn' onclick={this.handleclick}>Get writers</button>
-
-  </div> 
-  
-  </div>
-  </div>
-  )
 export default App
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,18 +1,14 @@
 import React from 'react'
 
-function profilecard(writer) {
+function ProfileCaard({card}) {
   return (
-    <div className='card'>
-        <div className='textgroup'>
-           <h3>{writer.name}</h3>
-           <p>{writer.email}</p>
-           <p>{writer.phone}</p>
-           <button className='actionBtn'>Read more</button>
-
-         </div>
-
+    <div className="greencard">
+    <h3>{card.firstname} {card.lastname}</h3>
+    <p>{card.email}</p>
+    <p>{card.phone}</p>
+      
     </div>
   )
 }
 
-export default profilecard
+export default ProfileCaard
